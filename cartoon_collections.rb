b04a446f400ea6bbde5a21fac2b495c9b243234a
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-14 02:42:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls)
   p "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|a| a.length > 4}
 end
 
 def find_the_cheese# code an argument here
